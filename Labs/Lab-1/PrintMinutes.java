8577df73f4b1efd4b0de89c61bfb18f54168e728
Mikhail Tavarez <miktavarez@gmail.com>
2017-10-20 07:56:08 -0400

    Add files via upload

---
+++ Labs/Lab-1/PrintMinutes.java
@@ -0,0 +1,10 @@
+
+public class PrintMinutes {
+
+	public static void main(String[] args) {
+		System.out.println("How do you measure, measure a year?");
+		System.out.println("It's got " +  60*24*365 + " minutes.");
+
+	}
+
+}
